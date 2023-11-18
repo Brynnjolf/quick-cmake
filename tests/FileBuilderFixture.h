5e84c6733c03872268f763a137e939180e20240a
Brynn <brynnengbers@gmail.com>
2023-11-18 13:14:09 +0100

    refactor unit tests. Add library test

---
+++ tests/FileBuilderFixture.h
@@ -0,0 +1,22 @@
+#include <catch2/catch_test_macros.hpp>
+#include <filesystem>
+
+class FileBuilderFixture
+{
+private:
+    const std::filesystem::path test_root_path = "temp";
+public:
+    FileBuilderFixture()
+    {
+        REQUIRE(std::filesystem::create_directory(test_root_path));
+    }
+    ~FileBuilderFixture()
+    {
+        REQUIRE(std::filesystem::remove_all(test_root_path));
+    }
+
+    std::filesystem::path rootPath()
+    {
+        return test_root_path;
+    }
+};
--- tests/test_FileBuilder.cpp
+++ tests/test_FileBuilder.cpp
@@ -1,8 +1,43 @@
 #include <catch2/catch_test_macros.hpp>
 #include <FileBuilder/FileBuilder.h>
+#include "FileBuilderFixture.h"
 
-TEST_CASE("basic-case")
+
+TEST_CASE_METHOD(FileBuilderFixture, "default executable creation")
 {
-    FileBuilder builder("testProject", ".", Type::Exe, Format::Default);
+
+    FileBuilder builder("testProject", rootPath(), Type::Exe, Format::Default);
     builder.create();
+
+    REQUIRE(std::filesystem::exists(rootPath() / "CMakeLists.txt"));
+    REQUIRE(std::filesystem::exists(rootPath() / "main.cpp"));
 }
+
+TEST_CASE_METHOD(FileBuilderFixture, "default library creation")
+{
+
+    FileBuilder builder("testProject", rootPath(), Type::Lib, Format::Default);
+    builder.create();
+
+    REQUIRE(std::filesystem::exists(rootPath() / "CMakeLists.txt"));
+    REQUIRE(std::filesystem::exists(rootPath() / "include/testProject/testProject.h"));
+    REQUIRE(std::filesystem::exists(rootPath() / "src/testProject.cpp"));
+}
+
+TEST_CASE_METHOD(FileBuilderFixture, "Qt Executable creation")
+{
+
+    FileBuilder builder("testProject", rootPath(), Type::Exe, Format::Qt);
+    builder.create();
+
+    REQUIRE(std::filesystem::exists(rootPath() / "CMakeLists.txt"));
+    REQUIRE(std::filesystem::exists(rootPath() / "main.cpp"));
+    REQUIRE(std::filesystem::exists(rootPath() / "mainwindow.cpp"));
+    REQUIRE(std::filesystem::exists(rootPath() / "mainwindow.h"));
+}
+
+
+
+
+
+
